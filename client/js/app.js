87b3259f38ecc63dfc2644a5428683477c0353a4
Markus Ullmark <ullmark@gmail.com>
2012-10-03 20:21:31 +0200

    render with a template instead

--- client/js/app.js
+++ client/js/app.js
@@ -6,27 +6,20 @@
 $(function() {
 
   var url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=lillerik&count=5";
+  var tmpl = Hogan.compile($("#tweets-tmpl").html());
 
   // Load the tweets...
   $.ajax({ url: url, dataType: "jsonp" })
+
     // ... // when fails
     .fail(function(response) {
       console.log(response);
     })
+
     // ... when done.
     .done(function(tweets) {
-      // Create a container for the tweets.
-      var ul = $("<ul>");
-      // We got some tweets, loop them and create the html
-      $.each(tweets, function(index, tweet) {
-        var li = $("<li>").html(tweet.text);
-        var img = $("<img>", { "src": tweet.user.profile_image_url, "alt": "" });
-        li.prepend(img);
-        ul.append(li);
-      });
-
-      // append it to the page in the correct place
-      $(".tweets .span6").append(ul);
+      // render the template
+      $(".tweets .span6").append(tmpl.render({ tweets: tweets }));
     });
 
 });
