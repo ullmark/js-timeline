43b663d271fa593e16afaa0327a8a9512e9393c4
Markus Ullmark <ullmark@gmail.com>
2012-10-03 20:32:18 +0200

    moved the code to a object, to encapsulate it

--- client/js/app.js
+++ client/js/app.js
@@ -2,24 +2,42 @@
 // Foo App
 // -----------
 
-// When DOM is loaded, load our tweets.
+// A first simple way of encapsulating your code in a
+// *namespace*
+var Foo = {
+
+  // ### init
+  init: function() {
+    this.tmpl = Hogan.compile($("#tweets-tmpl").html());
+    this.loadTweets();
+  },
+
+  // ### loadTweets
+  loadTweets: function() {
+    var url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=lillerik&count=5";
+
+    // store a reference to this.
+    var _this = this;
+
+    // Load the tweets...
+    $.ajax({ url: url, dataType: "jsonp" })
+
+      // ... when fails
+      .fail(function(response) {
+        console.log(response);
+      })
+
+      // ... when done.
+      .done(function(tweets) {
+        // render the template
+        $(".tweets .span6").append(_this.tmpl.render({ tweets: tweets }));
+      });
+  }
+
+};
+
+// When DOM is loaded, initialize our application
 $(function() {
-
-  var url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=lillerik&count=5";
-  var tmpl = Hogan.compile($("#tweets-tmpl").html());
-
-  // Load the tweets...
-  $.ajax({ url: url, dataType: "jsonp" })
-
-    // ... // when fails
-    .fail(function(response) {
-      console.log(response);
-    })
-
-    // ... when done.
-    .done(function(tweets) {
-      // render the template
-      $(".tweets .span6").append(tmpl.render({ tweets: tweets }));
-    });
-
+  Foo.init();
 });
+
